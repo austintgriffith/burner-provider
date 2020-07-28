17184ec284cf2a586abee8b82347c0727183237b
Tom French <tom@tomfren.ch>
2020-07-28 13:07:19 +0100

    refactor: split signer code into a different file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var ethers = require('ethers');
-var fs = require('fs');
+const { Web3Provider } = require('@ethersproject/providers');
 const ProviderEngine = require('web3-provider-engine')
 const CacheSubprovider = require('web3-provider-engine/subproviders/cache.js')
 const FixtureSubprovider = require('web3-provider-engine/subproviders/fixture.js')
@@ -10,15 +9,13 @@ const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracke
 const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
 const WebSocketSubProvider = require('web3-provider-engine/subproviders/websocket.js')
 
-const sigUtil = require('eth-sig-util')
+const sigUtil = require('eth-sig-util');
+const BurnerSigner = require('./signer');
 
 module.exports = BurnerProvider
 
-let metaAccount
-
 function BurnerProvider(opts = {}){
   var engine = new ProviderEngine()
-  let provider = new ethers.providers.Web3Provider(engine)
 
   // let them pass in a simple string for the options and use that as infura or whatevs
   if(typeof opts == "string"){
@@ -26,65 +23,7 @@ function BurnerProvider(opts = {}){
     opts = {rpcUrl}
   }
 
-  let privateKeyStorageString = "metaPrivateKey"
-  if(opts.namespace){
-    privateKeyStorageString = privateKeyStorageString+"_"+opts.namespace
-  }
-
-  if(opts&&opts.privateKey){
-    //if they passed in a private key, use it to generate an account
-    //metaAccount = provider.eth.accounts.privateKeyToAccount(opts.privateKey)
-    metaAccount = new ethers.Wallet(opts.privateKey, provider);
-    //console.log("metaAccount from pk",metaAccount)
-  } else if(typeof localStorage != "undefined"&&typeof localStorage.setItem == "function"){
-    //load private key out of local storage
-    let metaPrivateKey = localStorage.getItem(privateKeyStorageString)
-    if(metaPrivateKey=="0") metaPrivateKey=false;
-    if(metaPrivateKey && metaPrivateKey.length!==66) metaPrivateKey=false;
-    //if(metaPrivateKey) metaAccount = provider.eth.accounts.privateKeyToAccount(metaPrivateKey)
-    if(metaPrivateKey) metaAccount = new ethers.Wallet(metaPrivateKey, provider);
-  }else{
-    //local storage isn't an option and they didn't pass in a pk attempted to use the filesystem
-    try{
-      let fsPk = fs.readFileSync(".pk").toString()
-      if(fsPk){
-        metaAccount = new ethers.Wallet(fsPk, provider);
-      }
-    }catch(e){}
-    // if not just generate a temp account in memory for this session
-    // (just leave metaAccount false and it will be created in the next block)
-  }
-
-  if(!metaAccount){
-    //generate account either from a provided mnemonic, pk, or random generation
-    if(opts.mnemonic){
-      const bip39 = require('bip39');
-      const hdkey = require('ethereumjs-wallet/hdkey');
-      let index = "0"
-      if(typeof opts.index != "undefined"){
-        index = opts.index
-      }
-      const seed = bip39.mnemonicToSeedSync(opts.mnemonic);
-      const hdwallet = hdkey.fromMasterSeed(seed);
-      const wallet_hdpath = "m/44'/60'/0'/0/";
-      const wallet = hdwallet.derivePath(wallet_hdpath + index).getWallet();
-      const privateKey ="0x"+wallet.getPrivateKey().toString("hex")
-      //metaAccount = provider.eth.accounts.privateKeyToAccount(privateKey)
-      metaAccount = new ethers.Wallet(privateKey, provider);
-    }else{
-      //metaAccount = provider.eth.accounts.create();
-      metaAccount = ethers.Wallet.createRandom()
-    }
-    //if we needed to generate, save the pk to local storage
-    if(typeof localStorage != "undefined"&&typeof localStorage.setItem == "function"){
-      localStorage.setItem(privateKeyStorageString,metaAccount.privateKey)
-    }else{
-      //if we can't use local storage try saving it to the filesystem
-      try{
-        fs.writeFileSync(".pk",metaAccount.privateKey)
-      }catch(e){}
-    }
-  }
+  const metaAccount = BurnerSigner(opts).connect(new Web3Provider(engine))
 
   opts.getPrivateKey = (address,cb)=>{
     if(address.toLowerCase()==metaAccount.address.toLowerCase()){
@@ -95,7 +34,6 @@ function BurnerProvider(opts = {}){
   }
 
   opts.getAccounts = (cb)=>{
-    //console.log("metaAccount",metaAccount)
     cb(false,[metaAccount.address])
   }
 
